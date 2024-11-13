60eab111943a81294f0c93208edec46286e7772b
Shreya Karia <shreyakariaz@gmail.com>
2024-11-14 00:07:34 +0530

    Method to store a new chunk in a chunk server added.

--- src/internal/chunkserver/chunk_server.go
+++ src/internal/chunkserver/chunk_server.go
@@ -9,3 +9,15 @@ type ChunkServer struct {
 	ChunkData map[uint64][]byte // Stores chunk data, mapped by chunk ID
 	Mutex     sync.Mutex
 }
+
+// Method to store new chunk
+
+func (chunkServer *ChunkServer) storeChunkServer(chunkID uint64, chunkData []byte, isChunkCreated *bool) error {
+	chunkServer.Mutex.Lock()
+	defer chunkServer.Mutex.Unlock()
+
+	chunkServer.ChunkData[chunkID] = chunkData
+	*isChunkCreated = true
+
+	return nil
+}
